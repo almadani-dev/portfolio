e0cba96936935b3d507726c477684410a176a4be
almadani-dev <mohammed@almadani.dev>
2024-12-12 13:26:49 +0200

    'commit3'

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -20,7 +20,7 @@ import {
 import { ArrowUpRight, Github } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 
 const WorkPage = () => {
   return (
